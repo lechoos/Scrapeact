53a0b8e1e15503e37beba90a3260ca0e5755bd63
Piotr Lechowicz <ptrlechowicz@gmail.com>
2024-05-01 18:00:10 +0200

    added a regex

---
+++ client/utils/regex.ts
@@ -0,0 +1,1 @@
+export const URL_REGEX = /https:\/\/www\.google\.com\/maps\/search\/[A-Za-z]+\/@(([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?(,([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[Ee]([+-]?\d+))?)+)z\?entry=ttu/i;
