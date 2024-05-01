4a5a36499c5c4567a3bf1c4388aa1fd3616d529c
Piotr Lechowicz <ptrlechowicz@gmail.com>
2024-05-01 18:17:22 +0200

    created footer

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import styles from './footer.module.scss';
+
+export const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className={styles.footer}>
+      <p className={styles['footer__text']}>&copy; {year} Wszelkie prawa zastrzeżone</p>
+      <p className={styles.trademark}>Created by <a className={styles['trademark__link']} href="https://lotusite.pl" target="_blank" rel="noopener norefferer">Piotr Lechowicz</a></p>
+    </footer>
+  )
+};
