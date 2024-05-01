bb12272eefa0621ee351287adb018d17f47e6540
Piotr Lechowicz <ptrlechowicz@gmail.com>
2024-05-01 18:44:50 +0200

    added special corners

---
+++ client/src/components/Corner/Corner.tsx
@@ -0,0 +1,16 @@
+import styles from './corner.module.scss';
+
+interface CornerProps {
+  index: string;
+  rotate: number;
+}
+
+export const Corner: React.FC<CornerProps> = ({ index, rotate }) => {
+	return (
+		<div className={`${styles.corner} ${styles[`corner--${index}`]} ${styles[`rotate--${rotate}`]}`}>
+			<div className={`${styles.border} ${styles['border--top']}`}></div>
+			<div className={`${styles.border} ${styles['border--left']}`}></div>
+			<div className={styles.square}></div>
+		</div>
+	);
+};
