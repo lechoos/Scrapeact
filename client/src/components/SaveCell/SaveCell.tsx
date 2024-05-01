a734a4f91bf524c3a8a6d8d20abc65814d61b2b3
Piotr Lechowicz <ptrlechowicz@gmail.com>
2024-05-01 18:38:53 +0200

    added a Save Cell

---
+++ client/src/components/SaveCell/SaveCell.tsx
@@ -0,0 +1,22 @@
+import { MouseEvent } from 'react';
+import styles from './saveCell.module.scss';
+
+interface SaveProps {
+  checked: boolean;
+  size: number;
+  click: (e: EventTarget) => void
+}
+
+export const SaveCell: React.FC<SaveProps> = ({ checked, size, click }) => {
+  const text = checked ? 'Tak' : 'Nie';
+
+  const clickHandler = (e: MouseEvent<HTMLTableCellElement>) => {
+    click(e.target);
+  };
+
+  return (
+    <td onClick={clickHandler} width={size} className={styles['save-cell__container']}>
+      <span className={`${styles['save-cell']} ${checked ? styles['save-cell--true'] : styles['save-cell--false']}`}>{text}</span>
+    </td>
+  );
+};
