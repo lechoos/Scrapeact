378d14ffb0581c5a09f34573d6369b3bb5fbc623
Piotr Lechowicz <ptrlechowicz@gmail.com>
2024-05-04 05:42:29 +0200

    added a lottie loading json

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
-import './index.scss'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+);
