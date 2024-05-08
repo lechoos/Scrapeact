49386aa92b811f81e9db02dd8260b81c9d06bd0f
Piotr Lechowicz <ptrlechowicz@gmail.com>
2024-05-08 22:30:37 +0200

    fixed DeleteUserController so it also deletes all contacts of deleted user

--- server/controllers/DeleteUserController.js
+++ server/controllers/DeleteUserController.js
@@ -1,10 +1,12 @@
 const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 const DeleteUserController = async (req, res) => {
 	const { _id } = await req.body;
 
 	try {
 		await User.findOneAndDelete({ _id: _id });
+		await Contact.deleteMany({ ownerID: _id });
 
 		res.clearCookie('access-token');
 		res.clearCookie('user');
