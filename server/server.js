5cd336ba2c95822ed80103e8bab20eed09f32973
Piotr Lechowicz <ptrlechowicz@gmail.com>
2024-05-08 04:37:51 +0200

    deleted unnecessary imports

--- server/server.js
+++ server/server.js
@@ -2,10 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
-const User = require('./models/User');
-const { createTokens, validateToken } = require('./JWT');
 
 const ScrapingRoutes = require('./routes/ScrapingRoutes');
 const UserRoutes = require('./routes/UserRoutes');
